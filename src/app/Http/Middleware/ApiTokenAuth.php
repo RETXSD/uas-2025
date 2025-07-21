2f2116819f5b9bd5f473a5e62879a044fe8e5389
RETXSD <andrianandrian761@gmail.com>
2025-07-21 17:10:39 +0700

    uas

--- src/app/Http/Middleware/ApiTokenAuth.php
+++ src/app/Http/Middleware/ApiTokenAuth.php
@@ -17,13 +17,13 @@ class ApiTokenAuth
     public function handle(Request $request, Closure $next): Response
     {
         $token = $request->bearerToken();
-        $client = Client::where('api_token', $token)->first();
-        if (!$client) {
+        $pembeli = Client::where('api_token', $token)->first();
+        if (!$pembeli) {
             return response()->json([
                 'message' => 'Unauthorized'
             ], 401);
         }
-        $request->merge(['authenticated_client' => $client]);
+        $request->merge(['authenticated_pembeli' => $pembeli ]) ;
         return $next($request);
     }
 }
